3c4c83b6871414e37851273069e0dcdf872d43eb
Moein <moeinr516@gmail.com>
2025-12-15 19:10:10 +0330

    adding access modifiers and updating outputs and updating Q1 & Q3 methods

--- BoxPlots.java
+++ BoxPlots.java
@@ -1,56 +1,56 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Scanner;
-
+import java.util.*;
 public class BoxPlots {
-    double calculateQ1(List<Double> inputs) {
-        int element = (int) ((inputs.size() + 1) * (0.25) + 0.75);
-        return inputs.get(element - 1) + ((inputs.get(element) - inputs.get(element - 1)) * 0.25);
+    private double calculateQ1(List<Double> inputs) {
+        double element =  (inputs.size() + 1) * (0.25) + 0.5;
+        int distance = (int) element;
+        double p = element - distance;
+        return inputs.get(distance - 1) + ((inputs.get(distance) - inputs.get(distance - 1)) * p);
     }
 
-    double calculateQ3(List<Double> inputs) {
-        int element = (int) ((inputs.size() + 1) * (0.75) + 0.5);
-        return inputs.get(element - 2) + ((inputs.get(element - 1) - inputs.get(element - 2)) * 0.75);
+    private double calculateQ3(List<Double> inputs) {
+        double element =  inputs.size() * (0.75) + 0.25;
+        int distance = (int) element;
+        double p = element - distance;
+        return inputs.get(distance - 1) + ((inputs.get(distance) - inputs.get(distance - 1)) * p);
     }
 
-    double calculateLowOutLier(double q1, double q3) {
+    private double calculateLowOutLier(double q1, double q3) {
         double iqr = q3 - q1;
         return q1 - (1.5 * iqr);
     }
 
-    double calculateHighOutLier(double q1, double q3) {
+    private double calculateHighOutLier(double q1, double q3) {
         double iqr = q3 - q1;
         return q3 + (1.5 * iqr);
     }
 
-    double calculatingMedForEvenLength(List<Double> inputs) {
+   private double calculatingMedForEvenLength(List<Double> inputs) {
         double sum = inputs.get((inputs.size() / 2) - 1) + inputs.get((inputs.size() / 2));
         return sum / 2;
     }
 
-    double calculatingMedForOddLength(List<Double> inputs) {
+    private double calculatingMedForOddLength(List<Double> inputs) {
         return inputs.get(inputs.size() / 2);
     }
 
-    double min(List<Double> inputs) {
+    private double min(List<Double> inputs) {
         double lowOut = calculateLowOutLier(calculateQ1(inputs), calculateQ3(inputs));
         for (int i = 0; i < inputs.size(); i++) {
-            if (inputs.get(i) > lowOut) {
+            if (inputs.get(i) >= lowOut) {
                 return inputs.get(i);
             }
         }
         return inputs.getFirst();
     }
 
-    double max(List<Double> inputs) {
+    private double max(List<Double> inputs) {
         double highOut = calculateHighOutLier(calculateQ1(inputs), calculateQ3(inputs));
         double max = inputs.getLast();
-        if (inputs.getLast() < highOut)
+        if (inputs.getLast() <= highOut)
             return inputs.getLast();
         else {
             for (int i = inputs.size() / 2; i < inputs.size(); i++) {
-                if (inputs.get(i) < highOut)
+                if (inputs.get(i) <= highOut)
                     max = inputs.get(i);
                 else
                     return max;
@@ -59,13 +59,13 @@ public class BoxPlots {
         return -1;
     }
 
-    void invalidInputs() {
+    public void invalidInputs() {
         System.out.println("invalid input");
     }
 
-    List input() {
+    public ArrayList input() {
         int n = 0;
-        List<Double> inputs = new ArrayList<>();
+        ArrayList<Double> inputs = new ArrayList<>();
         BoxPlots plots = new BoxPlots();
         Scanner input = new Scanner(System.in);
         System.out.println("Enter number of your data: ");
@@ -89,10 +89,11 @@ public class BoxPlots {
         return inputs;
     }
 
-    void boxPlots(List<Double> inputs) {
+    protected void boxPlots(List<Double> inputs) {
         double med, q1, q3;
         int n = inputs.size();
         BoxPlots plots = new BoxPlots();
+        HashMap <String,Double> boxplots = new HashMap<String,Double>();
             if (n % 2 == 0)
                 med = plots.calculatingMedForEvenLength(inputs);
             else
@@ -101,13 +102,14 @@ public class BoxPlots {
             q3 = plots.calculateQ3(inputs);
             double lowOutLier = plots.calculateLowOutLier(q1, q3);
             double highOutLier = plots.calculateHighOutLier(q1, q3);
-            System.out.println("Minimum: " + plots.min(inputs));
-            System.out.println("Q1: " + q1);
-            System.out.println("Med: " + med);
-            System.out.println("Q3: " + q3);
-            System.out.println("Maximum: " + plots.max(inputs));
-            System.out.println("Low Outlier: " + lowOutLier);
-            System.out.println("High Outlier: " + highOutLier);
+            boxplots.put("Low Outlier ", lowOutLier);
+            boxplots.put("Minimum ", plots.min(inputs));
+            boxplots.put("Q1 ", q1);
+            boxplots.put("med ", med);
+            boxplots.put("Q3 ", q3);
+            boxplots.put("Maximum ", plots.max(inputs));
+            boxplots.put("High Outlier ", highOutLier);
+        System.out.println(boxplots);
     }
 
 
